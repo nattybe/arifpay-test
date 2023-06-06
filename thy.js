5a9427bb2833bc3e668fc99b24bec93b291c63c4
nattybe <82340052+nattybe@users.noreply.github.com>
2023-06-06 09:30:04 +0000

    made a butt

--- thy.js
+++ thy.js
@@ -1,29 +1,35 @@
-var myHeaders = new Headers();
-myHeaders.append("Authorization", "Bearer CHASECK-xxxxxxxxxxxxxxxx");
-myHeaders.append("Content-Type", "application/json");
-document.title="ArifPayment test"
-var raw = JSON.stringify({
-  "amount": "100",
-  "currency": "ETB",
-  "email": "abebech_bekele@gmail.com",
-  "first_name": "Bilen",
-  "last_name": "Gizachew",
-  "phone_number": "0912345678",
-  "tx_ref": "chewatatest-6669",
-  "callback_url": "https://webhook.site/077164d6-29cb-40df-ba29-8a00e59a7e60",
-  "return_url": "https://www.google.com/",
-  "customization[title]": "Payment for my favourite merchant",
-  "customization[description]": "I love online payments"
-});
 
-var requestOptions = {
-  method: 'POST',
-  headers: myHeaders,
-  body: raw,
-  redirect: 'follow'
-};
+function clickHandler(e) {
+  e.preventDefault();
+  let textref = document.getElementById('Reference');
+  textref = textref.target.value;
+  var myHeaders = new Headers();
+  myHeaders.append("Authorization", "Bearer CHASECK-xxxxxxxxxxxxxxxx");
+  myHeaders.append("Content-Type", "application/json");
+  document.title = "ArifPayment test"
+  var raw = JSON.stringify({
+    "amount": "100",
+    "currency": "ETB",
+    "email": "abebech_bekele@gmail.com",
+    "first_name": "Bilen",
+    "last_name": "Gizachew",
+    "phone_number": "0912345678",
+    "tx_ref": textref,
+    "callback_url": "https://webhook.site/077164d6-29cb-40df-ba29-8a00e59a7e60",
+    "return_url": "https://www.google.com/",
+    "customization[title]": "Payment for my favourite merchant",
+    "customization[description]": "I love online payments"
+  });
 
-fetch("https://api.chapa.co/v1/transaction/initialize", requestOptions)
-  .then(response => response.text())
-  .then(result => console.log(result))
-  .catch(error => console.log('error', error));
+  var requestOptions = {
+    method: 'POST',
+    headers: myHeaders,
+    body: raw,
+    redirect: 'follow'
+  };
+
+  fetch("https://api.chapa.co/v1/transaction/initialize", requestOptions)
+    .then(response => response.text())
+    .then(result => console.log(result))
+    .catch(error => console.log('error', error));
+}
