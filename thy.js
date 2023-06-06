98ab1ab1a464a846c758a4066d0e4f38bc5f70da
nattybe <themanintheomoon@gmail.com>
2023-06-06 12:04:08 +0300

    arif pay first try

---
+++ thy.js
@@ -0,0 +1,29 @@
+var myHeaders = new Headers();
+myHeaders.append("Authorization", "Bearer CHASECK-xxxxxxxxxxxxxxxx");
+myHeaders.append("Content-Type", "application/json");
+
+var raw = JSON.stringify({
+  "amount": "100",
+  "currency": "ETB",
+  "email": "abebech_bekele@gmail.com",
+  "first_name": "Bilen",
+  "last_name": "Gizachew",
+  "phone_number": "0912345678",
+  "tx_ref": "chewatatest-6669",
+  "callback_url": "https://webhook.site/077164d6-29cb-40df-ba29-8a00e59a7e60",
+  "return_url": "https://www.google.com/",
+  "customization[title]": "Payment for my favourite merchant",
+  "customization[description]": "I love online payments"
+});
+
+var requestOptions = {
+  method: 'POST',
+  headers: myHeaders,
+  body: raw,
+  redirect: 'follow'
+};
+
+fetch("https://api.chapa.co/v1/transaction/initialize", requestOptions)
+  .then(response => response.text())
+  .then(result => console.log(result))
+  .catch(error => console.log('error', error));
